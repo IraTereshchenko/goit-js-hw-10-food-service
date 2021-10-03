1fb1e58d445233bdc9b75d7dddb7e2873be364eb
IraTereshchenko <tereshchenkoim@gmail.com>
2021-10-03 12:53:53 +0300

    done

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+const refs = {
+  themeSwitchRef: document.querySelector('#theme-switch-toggle'),
+  bodyRef: document.querySelector('body'),
+  menuRef: document.querySelector('.js-menu'),
+};
+
+export const { themeSwitchRef, bodyRef, menuRef } = refs;
